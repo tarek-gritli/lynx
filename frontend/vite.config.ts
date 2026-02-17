51d2bdcb4acd9328b7e03d574c76995f193e1a8a
tarek-gritli <tarek.gritli@insat.ucar.tn>
2026-02-17 17:53:28 +0100

    feat: setup shadcn and add sidebar component

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
@@ -17,4 +18,9 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
